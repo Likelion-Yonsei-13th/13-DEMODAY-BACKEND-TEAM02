a2c5ae6c0c0a1c5061f5ac80b429ba36cb9a7022
younggyu7 <pyochang1@yonsei.ac.kr>
2025-11-26 13:01:06 +0900

    [Fix]베포 사이트 허용

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -214,6 +214,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "https://www.d-tour.kr",  # 프론트엔드 프로덕션
     "https://d-tour.kr",
+    "https://13-demoday-team-02.vercel.app",
 ]
 
 SECURE_SSL_REDIRECT = False  # HTTP 요청을 HTTPS로 리디렉트하는 것은 Nginx에서 이미 처리하므로 따로 진행하지 않음
